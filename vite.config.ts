34b0f714e6464d43df4e9339848cbc31e6ed0bd4
Erwan Jugand <erwan.jugand@gmail.com>
2023-02-09 23:43:37 +0100

    add build

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        popup: 'src/popup.html',
+        contentScript: 'src/contentScript.ts',
+        themes: 'src/themes.scss'
+      },
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js'
+      }
+    }
+  }
+})
