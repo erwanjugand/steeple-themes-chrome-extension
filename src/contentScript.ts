5dbf43a1bf12e6675b36579405422fab85b48884
Erwan Jugand <47392755+erwanjugand@users.noreply.github.com>
2023-02-10 20:22:17 +0100

    feat: Use vite with TypeScript + SCSS (#1)
    * add build
    
    * zip
    
    * add readme
    
    * chore(dependencies)

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,16 +1,17 @@
 console.info('Steeple themes : initialized')
 
-const setTheme = theme => {
+const setTheme = (theme: string) => {
   document.documentElement.dataset.steepleThemeExtension = theme
 }
 
 chrome.storage.local.get(['steeple-theme']).then(result => {
-  const value = result['steeple-theme']
+  const value: string | undefined = result['steeple-theme']
+  if (!value) { return }
   setTheme(value)
 })
 
 chrome.storage.onChanged.addListener(changes => {
-  const newValue = changes['steeple-theme'].newValue
+  const newValue: string | undefined = changes['steeple-theme'].newValue
   if (!newValue) { return }
   setTheme(newValue)
 });
--- src/popup.ts
+++ src/popup.ts
@@ -1,25 +1,27 @@
 document.addEventListener('DOMContentLoaded', async () => {
   // Translate popup
-  const ElementToTranslate = document.querySelectorAll('[data-i18n]')
+  const ElementToTranslate = document.querySelectorAll<HTMLElement>('[data-i18n]')
   ElementToTranslate.forEach(element => {
-    element.innerText = chrome.i18n.getMessage(element.dataset.i18n)
+    element.innerText = chrome.i18n.getMessage(element.dataset.i18n || '')
   })
 
   // Set version
-  const footerVersion = document.querySelector('.footer-version')
+  const footerVersion = document.querySelector<HTMLElement>('.footer-version')
   const { version } = await chrome.runtime.getManifest()
-  footerVersion.innerText = version
+  if (footerVersion) {
+    footerVersion.innerText = version
+  }
 
   // Get current theme
   await chrome.storage.local.get(['steeple-theme']).then(result => {
     const value = result['steeple-theme']
-    const activeAction = document.querySelector(`.action[data-theme="${value}"]`)
+    const activeAction = document.querySelector(`.main-action[data-theme="${value}"]`)
     if (!activeAction) { return }
     activeAction.classList.add('active')
   })
 
   // Set theme
-  const actions = document.querySelectorAll('.action')
+  const actions = document.querySelectorAll<HTMLButtonElement>('.main-action')
   actions.forEach(action => {
     action.addEventListener('click', async () => {
       const theme = action.dataset.theme
