86c6c103a7c8b3702e7a97c21935bc187a063285
Erwan Jugand <erwan.jugand@gmail.com>
2024-11-09 14:19:24 +0100

    chore(sass): fix deprecations

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
   build: {
     rollupOptions: {
       input: {
