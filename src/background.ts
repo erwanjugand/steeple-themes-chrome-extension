374288d675c5b3611d754889a15ab4662f5d29ea
Erwan Jugand <erwan.jugand@gmail.com>
2025-03-29 15:28:49 +0100

    feat: add contextMenu

---
+++ src/background.ts
@@ -0,0 +1,47 @@
+import { getTheme, onChangeTheme, setTheme, themes } from './themes'
+
+getTheme.then(currentTheme => {
+  chrome.contextMenus.create({
+    id: 'steeple-theme-selector',
+    title: chrome.i18n.getMessage('extensionName'),
+  })
+
+  themes.forEach(theme => {
+    chrome.contextMenus.create({
+      id: `steeple-theme-${theme}`,
+      parentId: 'steeple-theme-selector',
+      type: 'radio',
+      title: chrome.i18n.getMessage(`${theme}Action`),
+      checked: theme === currentTheme,
+    })
+  })
+})
+
+chrome.contextMenus.onClicked.addListener(({ parentMenuItemId, menuItemId }) => {
+  if (parentMenuItemId !== 'steeple-theme-selector') return
+
+  switch (menuItemId) {
+    case 'steeple-theme-dark':
+      setTheme('dark')
+      break
+    case 'steeple-theme-sepia':
+      setTheme('sepia')
+      break
+    default:
+      setTheme('default')
+      break
+  }
+})
+
+onChangeTheme(currentTheme => {
+  themes.forEach(theme => {
+    chrome.contextMenus.update(`steeple-theme-${theme}`, { checked: theme === currentTheme })
+  })
+})
+
+chrome.tabs.onHighlighted.addListener(async () => {
+  const currentTab = await chrome.tabs.query({ active: true, currentWindow: true })
+  const currentUrl = currentTab?.[0]?.url
+  const visible = currentUrl?.includes('steeple.fr') || currentUrl?.includes('steeple.com')
+  chrome.contextMenus.update('steeple-theme-selector', { visible })
+})
--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,21 +1,10 @@
+import { getTheme, onChangeTheme, type Theme } from './themes'
+
 console.info('Steeple themes : initialized')
 
-const setTheme = (theme: string) => {
+const setPageTheme = (theme: Theme) => {
   document.documentElement.dataset.steepleThemeExtension = theme
 }
 
-chrome.storage.local.get(['steeple-theme']).then((result: steepleThemeStorage) => {
-  const value: string | undefined = result['steeple-theme']
-  if (!value) {
-    return
-  }
-  setTheme(value)
-})
-
-chrome.storage.onChanged.addListener(changes => {
-  const newValue: string | undefined = changes['steeple-theme'].newValue
-  if (!newValue) {
-    return
-  }
-  setTheme(newValue)
-})
+getTheme.then(setPageTheme)
+onChangeTheme(setPageTheme)
